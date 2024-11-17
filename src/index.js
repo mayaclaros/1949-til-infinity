44b8b060614b0eb8d601d88a6b5dd0fafc997643
mayaclaros <mayawclaros@gmail.com>
2024-11-17 00:35:24 -0500

    Update js
    added AI generator to JS

---
+++ src/index.js
@@ -0,0 +1,31 @@
+function displayLyric(response) {
+  new Typewriter("#lyric", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+function generateLyric(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#instructions");
+  let apiKey = "24b9b3ae0f6ea728ao45f6et261b0962";
+  let prompt = `Answer a question about the year 1949 using facts and evidence  about ${instructionsInput.value} without a title that is 4 lines long and separate each line with a <br /> At the end of the answer. Sign the answer 'Happy 75th Birthday, Mom' inside a <strong> element at the end of the answer. Do not include a title`;
+  let context =
+    "User instructions: You are an expert in the year 1949 in the United States of America and Sountern African American culture. Your mission is to answer questions about the year 1949 that is 4 lines long in basic HTML";
+
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&key=${apiKey}`;
+
+  let lyricElement = document.querySelector("#lyric");
+  lyricElement.classList.remove("hidden");
+  lyricElement.innerHTML = `<div class="generating">⌛️ Generating a Rap Song about ${instructionsInput.value}</div>`;
+
+  console.log("generating lyric");
+  console.log(`Prompt:${prompt}`);
+  console.log(`Context: ${context}`);
+  axios.get(apiUrl).then(displayLyric);
+}
+
+let lyricFormElement = document.querySelector("#lyric-generator-form");
+lyricFormElement.addEventListener("submit", generateLyric);
